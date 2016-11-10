22ca5f0026e559043b67e8ec08b8428c90ae7405
Ihor Sviziev <igor@ven.com>
2016-11-10 16:11:47 +0200

    Format all php files according to PSR-2

--- src/app/code/community/EmPeWe/ApiLogger/Model/Server/Handler.php
+++ src/app/code/community/EmPeWe/ApiLogger/Model/Server/Handler.php
@@ -2,39 +2,37 @@
 
 class EmPeWe_ApiLogger_Model_Server_Handler extends Mage_Api_Model_Server_Handler
 {
-    private $boolLogActive  = false;
+    private $boolLogActive = false;
     private $boolLogVerbose = false;
-    private $strLogfile     = '';
-    
+    private $strLogfile = '';
+
     public function __construct()
     {
-        $this->boolLogActive  = Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_active');
+        $this->boolLogActive = Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_active');
         $this->boolLogVerbose = Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_verbose');
-        $this->strLogfile     = Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_file')
-                              ? Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_file')
-                              : 'EmPeWe_ApiLogger.log';
-        $this->forceLog        = Mage::getStoreConfig('apilogger_options/config/apilogger_force_log');
+        $this->strLogfile = Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_file')
+            ? Mage::getStoreConfig('apilogger_options/config/apilogger_v1_log_file')
+            : 'EmPeWe_ApiLogger.log';
+        $this->forceLog = Mage::getStoreConfig('apilogger_options/config/apilogger_force_log');
     }
 
     public function call($sessionId, $apiPath, $args = array())
     {
         $response = parent::call($sessionId, $apiPath, $args);
-        
-        if($this->boolLogActive)
-        {
+
+        if ($this->boolLogActive) {
             $log = "SOAP Method (V1): $apiPath";
-            $log.= "\ncalled from {$_SERVER['REMOTE_ADDR']}";
-            $log.= "\nParameters: " . print_r(array_merge((array)$sessionId, $args), true);
-            if($this->boolLogVerbose)
-            {
-                $log.= "\nResponse: " . print_r($response, true);
+            $log .= "\ncalled from {$_SERVER['REMOTE_ADDR']}";
+            $log .= "\nParameters: " . print_r(array_merge((array)$sessionId, $args), true);
+            if ($this->boolLogVerbose) {
+                $log .= "\nResponse: " . print_r($response, true);
             }
             Mage::log($log,
                 null,
                 $this->strLogfile,
                 $this->forceLog);
         }
-        
+
         return $response;
     }
-} // Class EmPeWe_ApiLogger_Model_Server_Handler End
+}
--- src/app/code/community/EmPeWe/ApiLogger/Model/Server/V2/Handler.php
+++ src/app/code/community/EmPeWe/ApiLogger/Model/Server/V2/Handler.php
@@ -2,39 +2,37 @@
 
 class EmPeWe_ApiLogger_Model_Server_V2_Handler extends Mage_Api_Model_Server_V2_Handler
 {
-    private $boolLogActive  = false;
+    private $boolLogActive = false;
     private $boolLogVerbose = false;
-    private $strLogfile     = '';
-    
+    private $strLogfile = '';
+
     public function __construct()
     {
-        $this->boolLogActive  = Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_active');
+        $this->boolLogActive = Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_active');
         $this->boolLogVerbose = Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_verbose');
-        $this->strLogfile     = Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_file')
-                              ? Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_file')
-                              : 'EmPeWe_ApiLogger.log';
-        $this->forceLog        = Mage::getStoreConfig('apilogger_options/config/apilogger_force_log');
+        $this->strLogfile = Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_file')
+            ? Mage::getStoreConfig('apilogger_options/config/apilogger_v2_log_file')
+            : 'EmPeWe_ApiLogger.log';
+        $this->forceLog = Mage::getStoreConfig('apilogger_options/config/apilogger_force_log');
     }
 
     public function __call($function, $args = array())
     {
         $response = parent::__call($function, $args);
-        
-        if($this->boolLogActive)
-        {
+
+        if ($this->boolLogActive) {
             $log = "SOAP Method (V2): $function";
-            $log.= "\ncalled from {$_SERVER['REMOTE_ADDR']}";
-            $log.= "\nParameters: " . print_r($args, true);
-            if($this->boolLogVerbose)
-            {
-                $log.= "\nResponse: " . print_r($response, true);
+            $log .= "\ncalled from {$_SERVER['REMOTE_ADDR']}";
+            $log .= "\nParameters: " . print_r($args, true);
+            if ($this->boolLogVerbose) {
+                $log .= "\nResponse: " . print_r($response, true);
             }
             Mage::log($log,
                 null,
                 $this->strLogfile,
                 $this->forceLog);
         }
-        
+
         return $response;
     }
-} // Class EmPeWe_ApiLogger_Model_Server_V2_Handler End
+}
